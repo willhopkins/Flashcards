378c3289e170ba04f523630102c78145c1fbb0aa
Will Hopkins <balrog13@gmail.com>
2012-11-22 19:22:33 -0500

    Added StudyModel.java and BuildView.java

---
+++ src/flashcards/BuildView.java
@@ -0,0 +1,14 @@
+package flashcards;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import java.awt.event.*;
+
+public class BuildView extends JFrame {
+
+}
