d59fa31561db45178091eff981f6d1c6a0b0f80c
Will Hopkins <balrog13@gmail.com>
2012-11-22 20:26:40 -0500

    Wrote out stubs in BuildView

--- src/flashcards/BuildView.java
+++ src/flashcards/BuildView.java
@@ -10,5 +10,55 @@ import javax.swing.SwingConstants;
 import java.awt.event.*;
 
 public class BuildView extends JFrame {
+	
+	private JButton quit, search, prev, next;
+	private JLabel question, answer;
+	private JTextField qText, aText;
+	
+	BuildView(Deck deck){
+		
+		this.deck = deck; //FIXME I'm clearly implementing something wrong here
+		
+		//Initialize all JButtons
+		quit = new JButton("Quit");
+		search = new JButton("Search");
+		prev = new JButton("Previous");
+		next = new JButton("Next");
+		
+		//Add ActionListeners to all JButtons
+		quit.addActionListener(new QuitListener());
+		search.addActionListener(new SearchListener());
+		prev.addActionListener(new PrevListener());
+		next.addActionListener(new NextListener());
+		
+	}
+	
+	private class QuitListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent unused) {
+			
+		}		
+	}
+	
+	private class SearchListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent unused) {
+			//Implement dialog box with JTextField for search string?
+		}
+	}
+	
+	private class PrevListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent unused) {
+			
+		}		
+	}
+	
+	private class NextListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent unused) {
+			
+		}
+	}
 
 }
